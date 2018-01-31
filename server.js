445370cd34dc32747f0d7373d2081f20143f4efa
Shawn Kim <16wk2@queensu.ca>
2018-01-31 04:28:18 -0500

    commit

--- server.js
+++ server.js
@@ -32,9 +32,7 @@ app.get("/managers", function(req, res) {
     var sent = false;
     for (var i = 0; i < employees.length; i++) {
         if (employees[i].isManager == true) {
-            if (sent) {return;}
             res.json(employees[i]);
-            sent = true;
         }
     }
     res.end();
