2816b955cf87e30c93b361d96992f23060eb9950
Shawn Kim <16wk2@queensu.ca>
2018-01-31 10:47:41 -0500

    commit

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require("express");
 var fs = require("fs");
-var http = require('http');
-var url = require('url');
 var data_service = require("./data-service");
 
 var HTTP_PORT = process.env.PORT || 8080;
@@ -10,47 +8,56 @@ var app = express();
 
 app.use(express.static('public')); 
 
-http.createServer(app, function(req, res) {
-   
-    if (req.url == '/home' || req.url == '/') {
-        fs.readFile('./views/home.html', 'UTF-8', function(err, data){
-            res.writeHead(200, { 'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    } else if (req.url == '/about'){
-        fs.readFile('./views/about.html', 'UTF-8', function(err, data){
-            res.writeHead(200, { 'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    } else if (req.url == '/employees') {
-        fs.readFile('./data/employees.json', 'utf8', function(err, data) {
-            data = JSON.parse(data);
-            res.json(data);
-            res.end();
-        });
-    } else if (req.url == '/managers') {
-        fs.readFile('./data/employees.json', 'utf8', function(err, data) {
-            data = JSON.parse(data);
-            var obj = [];
-            for (var i in data) {
-                if (data[i].isManager == true) {
-                    obj.push(data[i]);
-                }
-            }
+app.get("/", function(req, res) {
+    fs.readFile('./views/home.html', 'UTF-8', function(err, data){
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
 
-            res.json(obj);
-            res.end();
-        });
-    } else if (req.url == '/departments') {
-        fs.readFile('./data/departments.json', 'utf8', function(err, data) {
-            data = JSON.parse(data);
-            res.json(data);
-            res.end();
-        });
-    } else {
-        res.send("AAA");
-    }
-}).listen(8080);
+app.get("/about", function(req, res) {
+    fs.readFile('./views/about.html', 'UTF-8', function(err, data){
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end(data);
+    });
+});
+
+app.get("/employees", function(req, res) {
+    fs.readFile('./data/employees.json', 'utf8', function(err, data) {
+        data = JSON.parse(data);
+        res.json(data);
+        res.end();
+    });
+});
+
+app.get("/managers", function(req, res) {
+    fs.readFile('./data/employees.json', 'utf8', function(err, data) {
+        data = JSON.parse(data);
+        var obj = [];
+        for (var i in data) {
+            if (data[i].isManager == true) {
+                obj.push(data[i]);
+            }
+        }
+
+        res.json(obj);
+        res.end();
+    });
+});
+
+app.get("/departments", function(req, res) {
+    fs.readFile('./data/departments.json', 'utf8', function(err, data) {
+        data = JSON.parse(data);
+        res.json(data);
+        res.end();
+    });
+});
+
+app.get('/!about|employees|managers|departments', function(req, res) {
+    res.status(404);
+    res.send("Page Not Found");
+    res.end();
+})
 
 function onhttpstart(){
     console.log('Express http server listening on: ' + HTTP_PORT);
