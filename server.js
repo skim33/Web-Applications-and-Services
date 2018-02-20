e9c51726b88f1508f523957c194d9164045cd9ba
Shawn Kim <16wk2@queensu.ca>
2018-02-19 20:19:06 -0500

    commit

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get("/employees", function(req, res) {
 });
 
 app.get("/employee/:num", function(req, res) {
-    data_service.getEmployeeByNum(req.params.num).then(function(list){
+    data.getEmployeeByNum(req.params.num).then(function(list){
         res.json(list);
     }).catch(function(err) {
         res.jason({message: err});
