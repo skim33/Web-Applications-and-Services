c2693e7e2efe8d7bd5e0e47227d417e5f87dca81
Shawn Kim <16wk2@queensu.ca>
2018-03-11 00:18:24 -0500

    commit

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.post("/employees/add", function(req, res) {
 
 app.post("/employee/update", function(req, res) {
     console.log(req.body);         
-    data_service.updateEmployee(req.body).then(function(data) {
+    data_service.updateEmployee(req.body).then(function() {
         res.redirect("/employees");
     }).catch(function(err) {
         res.json({message: err});
