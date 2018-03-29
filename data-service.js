feca37a66f295792b12a1d50bfb83311c85972b1
Shawn Kim <16wk2@queensu.ca>
2018-03-29 01:47:44 -0400

    commit

--- data-service.js
+++ data-service.js
@@ -106,7 +106,6 @@ module.exports.addEmployee = function(employeeData) {
                 }
             }
             resolve(Employee.create({
-                employeeNum: employeeNum.employeeNum,
                 firstName: employeeData.firstName,
                 lastName: employeeData.lastName,
                 email: employeeData.email,
