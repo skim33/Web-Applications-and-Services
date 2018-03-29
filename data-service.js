cf6e8798cfa2a073f428347f95588507ed491ccb
Shawn Kim <16wk2@queensu.ca>
2018-03-28 23:24:47 -0400

    commit

--- data-service.js
+++ data-service.js
@@ -26,6 +26,7 @@ sequelize.authenticate().then(function() {
 });
 
 const Employee = sequelize.define('Employee', {
+    tableName: 'Employee',
     employeeNum: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
     firstName: Sequelize.STRING,
     lastName: Sequelize.STRING,
@@ -47,6 +48,7 @@ const Employee = sequelize.define('Employee', {
 });
 
 const Department = sequelize.define("Department", {
+    tableName: 'Department',
     departmentId: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
     departmentName: Sequelize.STRING}, {
         createdAt: false,
