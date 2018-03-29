cedb7d17f41a93739f46bac32e55eaff796bf646
Shawn Kim <16wk2@queensu.ca>
2018-03-29 00:33:51 -0400

    commit

--- data-service.js
+++ data-service.js
@@ -97,6 +97,7 @@ module.exports.getDepartments = function() {
 
 module.exports.addEmployee = function(employeeData) {
     employeeData.isManager = (employeeData.isManager) ? true : false;
+    console.log(employeeData.employeeNum);
     return new Promise(function(resolve, reject) {
         sequelize.sync().then(function() {
             for(var i in employeeData) {
@@ -194,6 +195,7 @@ module.exports.updateEmployee = function(employeeData) {
                 }
             }
             resolve(Employee.create({
+                employeeNum: employeeData.employeeNum,
                 firstName: employeeData.firstName,
                 lastName: employeeData.lastName,
                 email: employeeData.email,
